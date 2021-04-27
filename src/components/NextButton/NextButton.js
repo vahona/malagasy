7b5ee0ddc6a18b6a2a7c8d129de4bb62cab068ef
Zafimarovololona Belle Sugi <sugi.zafi@onja.org>
2021-04-27 15:22:10 +0300

    Creating four version of button

--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TouchableHighlight } from 'react-native';
+import { TouchableHighlight, StyleSheet } from 'react-native';
 
-export default function NextButton({ onPress, children }) {
-    return <TouchableHighlight onPress={onPress}>{children}</TouchableHighlight>;
+
+const styles = StyleSheet.create({
+    disabled: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#06B6D4',
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        borderRadius: 30,
+        width: 90,
+        height: 40,
+        borderColor: '#06B6D4',
+        borderWidth: 1,
+        margin: 16
+
+    },
+
+    able: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(6, 182, 212, 1)',
+        color: 'rgba(255, 255, 255, 1)',
+        borderRadius: 30,
+        width: 90,
+        height: 40,
+        margin: 16
+    }
+})
+
+function NextAdd() {
+    return alert('next')
 }
 
-Button.defaultProps = {
+function returnNull() {
+    return null
+}
+
+export default function NextButton({ onPress, children, disable = true }) {
+    return <TouchableHighlight
+        onPress={disable ? returnNull : NextAdd}
+        style={disable ? styles.disabled : styles.able}
+    >{children}</TouchableHighlight>;
+}
+
+NextButton.defaultProps = {
     children: null,
     onPress: () => { },
 };
 
-Button.propTypes = {
+NextButton.propTypes = {
     children: PropTypes.node,
     onPress: PropTypes.func,
 };
--- src/components/NextButton/NextButton.stories.js
+++ src/components/NextButton/NextButton.stories.js
@@ -1,20 +1,51 @@
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
+import { color, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import NextButton from './NextButton';
+
+
+const styles = StyleSheet.create({
+    textdisabled: {
+        color: '#06B6D4'
+    },
+
+    textable: {
+        color: '#FFFFFF'
+    },
+
+})
+
+function Disabled() {
+    const [disable, setDisable] = useState(true);
+    return (
+        <NextButton
+            disable={true}
+        ></NextButton>
+    );
+}
 
 
 storiesOf('NextButton', module)
     .addDecorator((getStory) => <View>{getStory()}</View>)
     .add('disableAdd', () => (
-        <NextButton onPress={action('clicked-text')}>
-            <Text>{text('Add')}</Text>
+        <NextButton onPress={action('clicked-text')} disable={true}>
+            <Text style={styles.textdisabled} >Add</Text>
         </NextButton>
     ))
     .add('ableAdd', () => (
-        <NextButton onPress={action('clicked-emoji')}>
-            <Text>{text('Add')} </Text>
+        <NextButton onPress={action('clicked-emoji')} disable={false}>
+            <Text style={styles.textable}>Add</Text>
         </NextButton>
-    ));
+    ))
+    .add('disableNext', () => (
+        <NextButton onPress={action('clicked-emoji')} disable={true}>
+            <Text style={styles.textdisabled} >Next</Text>
+        </NextButton>
+    ))
+    .add('ableNext', () => (
+        <NextButton onPress={action('clicked-emoji')} disable={false}>
+            <Text style={styles.textable}>Next</Text>
+        </NextButton>
+    ))
